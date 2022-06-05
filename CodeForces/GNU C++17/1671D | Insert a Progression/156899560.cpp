6008b14215db91aa58d59b4929fda47a1e613300
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:09:33 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1671D | Insert a Progression/156899560.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define sz(x) ((ll) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define memo(x,val) memset(x,val,sizeof(x))
+#define put(n) cout<<(n)<<endl
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<64>(n).to_string()
+#define toBinary(n) stoll(bitset<64>(n).to_string())
+ 
+// debugging
+ 
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl
+#define debx(x) cout << #x << " = " << (x) << endl
+#define deb2(a,b) cout<<#a<<" = "<<(a)<<", "<<#b<<" = "<<(b)<<endl
+#define deb3(a,b,c) cout<<#a<<" = "<<(a)<<", "<<#b<<" = "<<(b)<<", "<<#c<<" = "<<(c)<<endl
+#define debv(v) cout<<#v<<" = "; for(auto x: v) {cout<<x<<", ";} cout<<endl;
+#define debv2(v) for(int i=0; i<v.size(); i++) {cout<<#v<<"["<<i<<"] = "<<v[i]<<endl;}
+ 
+template<typename... T>
+void debm(T&&... args) {
+    ((cout << args << ", "), ...);
+    cout << endl;
+}
+ 
+/*
+    ---- take notes here ----
+    
+*/
+ 
+void solve()
+{
+    ll n,x; cin>>n>>x;
+    vll v; readl(v,n);
+ 
+    ll s=0;
+    ll ans=inf;
+ 
+    for(ll i=0; i<n-1; i++)
+    {
+        s+=abs(v[i]-v[i+1]);
+    }
+ 
+    for(ll k=1; k<=2; k++)
+    {
+        ll min1 = abs(v[0]-1);
+ 
+        ans = min(ans, s + abs(v[0]-x) + (x-1));
+ 
+        for(ll i=0; i<n-1; i++)
+        {
+            ans = min(ans, s - abs(v[i]-v[i+1]) + min1 + abs(v[i]-x) + abs(x-v[i+1]));
+ 
+            ans = min(ans, s - abs(v[i]-v[i+1]) + abs(v[i]-1) + (x-1) + abs(x-v[i+1]));
+ 
+            min1 = min(min1, abs(v[i]-1) + abs(1-v[i+1]) - abs(v[i]-v[i+1]));
+        }
+ 
+        ans = min(ans, s + min1 + abs(v.back()-x));
+ 
+        reverse(all(v));
+    }
+ 
+    cout<<ans<<endl;
+}
+ 
+int main()
+{
+    FAST
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();    
+    }
+ 
+    return 0;
+}
