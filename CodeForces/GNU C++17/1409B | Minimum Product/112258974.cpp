ff62cfaf8199efe057fdf371100d1901a35bcba0
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:32:06 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1409B | Minimum Product/112258974.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define sz(x) ((ll) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl
+#define debx(x) cout << #x << " = " << (x) << endl
+#define deb2(a,b) cout<<(a)<<"----"<<(b)<<endl
+#define debv(v) for(auto x: v) {cout<<x<<", ";} cout<<endl;
+#define put(n) cout<<(n)<<endl
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<64>(n).to_string()
+#define toBinary(n) stoll(bitset<64>(n).to_string())
+ 
+/*
+    ---- take notes here ----
+    
+*/
+ 
+void solve()
+{
+    ll a,b,x,y,n; cin>>a>>b>>x>>y>>n;
+ 
+    ll adiff=a-x, bdiff=b-y;
+    //deb2(adiff,bdiff);
+ 
+    ll n1=n;
+    ll a1=a-min(adiff,n1);
+    n1-=min(adiff,n1);
+    ll b1=b-min(bdiff,n1);
+    ll res1=a1*b1;
+    //deb2(a1,b1);
+ 
+    ll n2=n;
+    ll b2=b-min(bdiff,n2);
+    n2-=min(bdiff,n2);
+    ll a2=a-min(adiff,n2);
+    ll res2=a2*b2;
+    //deb2(a2,b2);
+ 
+    cout<<min(res1,res2)<<endl;
+}
+ 
+int main()
+{
+    FAST
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();    
+    }
+ 
+    return 0;
+}
