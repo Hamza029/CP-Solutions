dc93d253c8070b7f82345cbe6f16087303ed8375
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:35:21 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1467A | Wizard of Orz/103771276.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end());
+#define MAX(v) *max_element(v.begin(), v.end());
+#define sz(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl;
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<8>(n).to_string()
+#define toBinary(n) stoi(bitset<8>(n).to_string())
+ 
+void solve()
+{
+ int n; cin>>n;
+ int cnt=8,i;
+ string s="98";
+ int mn=min(2,n);
+ for(i=1;i<=mn;i++)
+ {
+  cout<<s[i-1];
+ }
+ for(i=mn+1;i<=n;i++)
+ {
+  cnt++;
+  if(cnt>9) cnt=0;
+  cout<<cnt;
+ }
+ cout<<endl;
+}
+ 
+int main()
+{
+ int t = 1;
+ cin >> t;
+ while(t--)
+ {
+  solve(); 
+ }
+ 
+ return 0;
+}
