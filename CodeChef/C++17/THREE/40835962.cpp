420a5ce475666960f4b3d0bc72d53b786ddb37f3
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:07:24 +0600

    Committing files

---
+++ CodeChef/C++17/THREE/40835962.cpp
@@ -0,0 +1,73 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define read(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end());
+#define MAX(v) *max_element(v.begin(), v.end());
+#define sz(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 10000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1e18
+#define ninf -1e18
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<8>(n).to_string()
+#define toBinary(n) stoi(bitset<8>(n).to_string())
+
+void solve()
+{
+    string s;
+    cin >> s;
+    map <char, ll> mp;
+    ll len = s.length();
+    rep(i,0,len)
+    {
+        mp[s[i]]++;
+    }
+    ll cnt = 0;
+    for(auto i: mp)
+    {
+        //ll d = i.ss/2;
+        //cnt += d;
+        if(i.ss >= 2)
+        {
+            while(len >= 3 && i.ss >= 2)
+            {
+                len -= 3;
+                i.ss -= 2;
+                cnt++;
+            }
+        }
+    }
+    cout << cnt << endl;
+}
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while(t--)
+        solve();
+}
+
+
+
+
+
+
