4c8d0523bd59a97d8399d7bc0e9073ff176a6b86
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:35:04 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1478B | Nezzar and Lucky Number/105735248.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end());
+#define MAX(v) *max_element(v.begin(), v.end());
+#define sz(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000
+#define ninf -1000000000000000000
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl;
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<8>(n).to_string()
+#define toBinary(n) stoi(bitset<8>(n).to_string())
+ 
+bool contains(ll n, ll d)
+{
+ while(n)
+ {
+  if(n%10 == d) return 1;
+  n/=10;
+ }
+ return 0;
+}
+ 
+void solve()
+{
+ ll n,d; cin>>n>>d;
+ vll v;
+ rep(i,0,n)
+ {
+  ll val; cin>>val;
+  bool ok=0;
+  while(val>=0)
+  {
+   if(contains(val,d)) {cout<<"YES"<<endl; ok=1; break;}
+   val-=d;
+  }
+  if(!ok) cout<<"NO"<<endl;
+ }
+}
+ 
+int main()
+{
+ int t = 1;
+ cin >> t;
+ while(t--)
+ {
+  solve(); 
+ }
+ 
+ return 0;
+}
