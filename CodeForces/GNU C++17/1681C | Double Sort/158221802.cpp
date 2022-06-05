e63a3c510b0fe4e9ad255ccedcd57828773ba790
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:08:49 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1681C | Double Sort/158221802.cpp
@@ -0,0 +1,125 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define sz(x) ((ll) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define memo(x,val) memset(x,val,sizeof(x))
+#define put(n) cout<<(n)<<endl
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<64>(n).to_string()
+#define toBinary(n) stoll(bitset<64>(n).to_string())
+ 
+// debugging
+ 
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl
+#define debx(x) cout << #x << " = " << (x) << endl
+#define deb2(a,b) cout<<#a<<" = "<<(a)<<", "<<#b<<" = "<<(b)<<endl
+#define deb3(a,b,c) cout<<#a<<" = "<<(a)<<", "<<#b<<" = "<<(b)<<", "<<#c<<" = "<<(c)<<endl
+#define debv(v) cout<<#v<<" = "; for(auto x: v) {cout<<x<<", ";} cout<<endl;
+#define debv2(v) for(int i=0; i<v.size(); i++) {cout<<#v<<"["<<i<<"] = "<<v[i]<<endl;}
+ 
+template<typename... T>
+void debm(T&&... args) {
+    ((cout << args << ", "), ...);
+    cout << endl;
+}
+ 
+/*
+    ---- take notes here ----
+    
+*/
+ 
+void solve()
+{
+    ll n; cin>>n;
+    vll a; readl(a,n);
+    vll b; readl(b,n);
+ 
+    vector<pll> vec,ans;
+    map<ll,ll> m;
+ 
+    vll a1=a,b1=b;
+    srt(a1); srt(b1);
+ 
+    for(auto x: a) m[x]++;
+    for(auto x: b) m[x]++;
+ 
+    for(ll i=0; i<n; i++)
+    {
+        vec.pb(mp(a[i],b[i]));
+    }
+ 
+    sort(all(vec));
+ 
+    for(ll i=0; i<n; i++)
+    {
+        ll x=m[a[i]],y=m[b[i]];
+ 
+        if(a[i]!=vec[i].ff || b[i]!=vec[i].ss)
+        {
+            for(ll k=i+1; k<n; k++)
+            {
+                if(a[k]==vec[i].ff && b[k]==vec[i].ss)
+                {
+                    swap(a[i],a[k]);
+                    swap(b[i],b[k]);
+                    ans.pb(mp(i,k));
+                }
+            }
+        }
+    }
+ 
+    for(ll i=1; i<n; i++)
+    {
+        if(vec[i].ff<vec[i-1].ff || vec[i].ss<vec[i-1].ss)
+        {
+            cout<<-1<<endl;
+            return;
+        }
+    }
+ 
+    cout<<sz(ans)<<endl;
+    for(auto x: ans)
+    {
+        cout<<x.ff+1<<" "<<x.ss+1<<endl;
+    }
+}
+ 
+int main()
+{
+    FAST
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();    
+    }
+ 
+    return 0;
+}
