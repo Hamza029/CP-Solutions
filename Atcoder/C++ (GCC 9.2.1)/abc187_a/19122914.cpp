6b64d3fab12f74e9e2610c1c3fb781c02e820739
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:05:08 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc187_a/19122914.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define read(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end());
+#define MAX(v) *max_element(v.begin(), v.end());
+#define sz(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl;
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<8>(n).to_string()
+#define toBinary(n) stoi(bitset<8>(n).to_string())
+
+void solve()
+{
+	vector<pair<double,double>> v;
+	int n;
+	cin >> n;
+	rep(i,0,n)
+	{
+		double x,y; cin >> x >> y;
+		v.pb(mp(x,y));
+	}
+	int cnt = 0;
+	rep(i,0,n)
+	{
+		rep(j,i+1,n)
+		{
+			double m = (v[i].ss-v[j].ss)/(v[i].ff-v[j].ff);
+			if(m >= -1 && m <= 1) cnt++;
+		}
+	}
+	cout << cnt << endl;
+}
+
+int main()
+{
+	int t = 1;
+	//cin >> t;
+	while(t--)
+	{
+		solve();	
+	}
+
+	return 0;
+}
