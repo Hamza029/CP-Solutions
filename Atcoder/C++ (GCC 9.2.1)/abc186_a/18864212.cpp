1409678ed04a2bc54d7130a4ad89d8b42038bd46
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:05:52 +0600

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc186_a/18864212.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define read(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 10000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1e18
+#define ninf -1e18
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<8>(n).to_string()
+#define toBinary(n) stoi(bitset<8>(n).to_string())
+
+int main()
+{
+	int n, w;
+	cin >> n >> w;
+	int res = n/w;
+	cout << res << endl;
+
+	return 0;
+}
