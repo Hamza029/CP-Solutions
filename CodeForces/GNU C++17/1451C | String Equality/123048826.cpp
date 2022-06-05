3f757a8670eeb9a140d4cdaaa0b1f4ade722797d
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:25:23 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1451C | String Equality/123048826.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define sz(x) ((ll) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl
+#define debx(x) cout << #x << " = " << (x) << endl
+#define deb2(a,b) cout<<(a)<<" == "<<(b)<<endl
+#define debv(v) for(auto x: v) {cout<<x<<", ";} cout<<endl;
+#define put(n) cout<<(n)<<endl
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<64>(n).to_string()
+#define toBinary(n) stoll(bitset<64>(n).to_string())
+ 
+/*
+    ---- take notes here ----
+    
+*/
+ 
+string a,b;
+ll n,k;
+ 
+void solve()
+{
+    cin>>n>>k;
+    cin>>a>>b;
+    ll ma[205]={0},mb[205]={0};
+    for(ll i=0; i<n; i++) ma[a[i]]++,mb[b[i]]++;
+    for(char i='a'; i<'z'; i++)
+    {
+        ll diff=ma[i]-mb[i];
+        if(diff>=0 && diff%k==0)
+        {
+            ma[(char)(i+1)]+=diff;
+            ma[i]-=diff;
+        }
+        else
+        {
+            cout<<"No"<<endl;
+            return;
+        }
+    }
+    for(char i='a'; i<='z'; i++)
+    {
+        // deb2(i,ma[i]);
+        if(ma[i]!=mb[i])
+        {
+            cout<<"No"<<endl;
+            return;
+        }
+    }
+    cout<<"Yes"<<endl;
+}
+ 
+int main()
+{
+    FAST
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();    
+    }
+ 
+    return 0;
+}
