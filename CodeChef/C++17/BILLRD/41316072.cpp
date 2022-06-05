41507c4a67ee2c4b3eb93a0d2f2b991f86b0aa56
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:07:15 +0600

    Committing files

---
+++ CodeChef/C++17/BILLRD/41316072.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end());
+#define MAX(v) *max_element(v.begin(), v.end());
+#define sz(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl;
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<8>(n).to_string()
+#define toBinary(n) stoi(bitset<8>(n).to_string())
+
+void solve()
+{
+	vector <pll> v;
+	//v.pb(pll(-1,-1));
+	ll n, k, x, y; cin >> n >> k >> x >> y;
+	//if(k >= n) k = k%4;
+	{
+		if(x == y)
+		{
+			cout << n << " " << n << endl;
+			return;
+		}
+		else if(x > y)
+		{
+			v.pb(pll(n,n-x+y));
+			v.pb(pll(n-x+y,n));
+			v.pb(pll(0,x-y));
+			v.pb(pll(x-y,0));
+			k = k%4;
+			if(k == 0) k = 3;
+			else k--;
+			cout << v[k].ff << " " << v[k].ss << endl;
+			return;
+		}
+		else if(x < y)
+		{
+			v.pb(pll(x+n-y,n));
+			v.pb(pll(n,x+n-y));
+			v.pb(pll(y-x,0));
+			v.pb(pll(0,y-x));
+			k = k%4;
+			if(k == 0) k = 3;
+			else k--;
+			cout << v[k].ff << " " << v[k].ss << endl;
+			return;
+		}
+	}
+}
+
+int main()
+{
+	int t = 1;
+	cin >> t;
+	while(t--)
+	{
+		solve();	
+	}
+
+	return 0;
+}
