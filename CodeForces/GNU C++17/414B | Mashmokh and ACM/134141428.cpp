9341f6ed6f9c4b0ee9eade915673d978794ee094
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:19:08 +0600

    Committing files

---
+++ CodeForces/GNU C++17/414B | Mashmokh and ACM/134141428.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define sz(x) ((ll) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl
+#define debx(x) cout << #x << " = " << (x) << endl
+#define deb2(a,b) cout<<#a<<" = "<<(a)<<", "<<#b<<" = "<<(b)<<endl
+#define debv(v) cout<<#v<<" = "; for(auto x: v) {cout<<x<<", ";} cout<<endl;
+#define debv2(v) for(int i=0; i<v.size(); i++) {cout<<#v<<"["<<i<<"] = "<<v[i]<<endl;}
+#define put(n) cout<<(n)<<endl
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<64>(n).to_string()
+#define toBinary(n) stoll(bitset<64>(n).to_string())
+ 
+/*
+    ---- take notes here ----
+    
+*/
+ 
+void solve()
+{
+    ll n,k; cin>>n>>k;
+    ll dp[2005][2005];
+    memset(dp,0,sizeof(dp));
+    for(ll i=1; i<=n; i++)
+    {
+        dp[1][i]=1;
+    }
+    for(ll i=2; i<=k; i++)
+    {
+        for(ll j=1; j<=n; j++)
+        {
+            for(ll div=j; div<=n; div+=j)
+            {
+                dp[i][div]+=dp[i-1][j];
+                dp[i][div]%=MOD;
+            }
+        }
+    }
+    ll ans=0;
+    for(ll i=1; i<=n; i++)
+    {
+        ans+=dp[k][i];
+        ans%=MOD;
+    }
+    cout<<ans<<endl;
+}
+ 
+int main()
+{
+    FAST
+    int t = 1;
+    // cin >> t;
+    while(t--)
+    {
+        solve();    
+    }
+ 
+    return 0;
+}
