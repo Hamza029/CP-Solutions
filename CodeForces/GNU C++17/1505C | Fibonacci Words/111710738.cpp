6e71aef79a0b336b8e5a96c5ddb17f811f83559b
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-06-06 03:32:30 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1505C | Fibonacci Words/111710738.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define sz(x) ((ll) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl
+#define debx(x) cout << #x << " = " << (x) << endl
+#define deb2(a,b) cout<<(a)<<"----"<<(b)<<endl
+#define debv(v) for(auto x: v) {cout<<x<<", ";} cout<<endl;
+#define put(n) cout<<(n)<<endl
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<64>(n).to_string()
+#define toBinary(n) stoll(bitset<64>(n).to_string())
+ 
+/*
+ ---- take notes here ----
+ 
+*/
+ 
+void solve()
+{
+ map<char,int> m,m2;
+ int cnt=1;
+ for(char i='A'; i<='Z'; i++)
+ {
+  m[i]=cnt;
+  m2[i]=cnt+1;
+  cnt++;
+ }
+ //for(auto x: m) deb2(x.ff,x.ss);
+ string s; cin>>s;
+ ll len=sz(s);
+ if(len<=2)
+ {
+  cout<<"YES"<<endl;
+  return;
+ }
+ for(ll i=2; i<len; i++)
+ {
+  ll rem=(m[s[i-1]]+m[s[i-2]])-1;
+  if(rem>26)rem=rem%26;
+  //if(rem==0) rem=27;
+  //debx(rem);
+  if(rem==m[s[i]]) continue;
+  else
+  {
+   cout<<"NO"<<endl;
+   return;
+  }
+ }
+ cout<<"YES"<<endl;
+}
+ 
+int main()
+{
+ int t = 1;
+ //cin >> t;
+ while(t--)
+ {
+  solve(); 
+ }
+ 
+ return 0;
+}
