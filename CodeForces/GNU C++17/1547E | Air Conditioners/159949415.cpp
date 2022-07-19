1ec64ebf99dd39ca422cc5bcc41645856f19bda3
Golam Ibna Hamza <88612868+Hamza029@users.noreply.github.com>
2022-07-20 01:49:10 +0600

    Committing files

---
+++ CodeForces/GNU C++17/1547E | Air Conditioners/159949415.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+#define FAST ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pi 2*acos(0.0)
+#define readl(v,n) for(ll i=0;i<n;i++) {ll val; cin>>val; v.pb(val);}
+#define readi(v,n) for(int i=0;i<n;i++) {int val; cin>>val; v.pb(val);}
+#define srt(v) sort(v.begin(), v.end());
+#define rsrt(v) sort(v.rbegin(), v.rend());
+#define MIN(v) *min_element(v.begin(), v.end())
+#define MAX(v) *max_element(v.begin(), v.end())
+#define sz(x) ((ll) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define rep(i, a, b) for(ll i = (a); i < (b); i++)
+#define rep2(i, a, b) for(ll i = (a); i <= (b); i++)
+#define vll vector <ll>
+#define vii vector <int>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define M 1000007
+#define MOD 1000000007
+#define pb push_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define endl "\n"
+#define inf 1000000000000000000
+#define ninf -1000000000000000000
+#define memo(x,val) memset(x,val,sizeof(x))
+#define put(n) cout<<(n)<<endl
+#define toDecimal(s) bitset<64>(s).to_ullong()
+#define toBinaryStr(n) bitset<64>(n).to_string()
+#define toBinary(n) stoll(bitset<64>(n).to_string())
+ 
+// debugging
+ 
+#define deb(n) cout<<"--"<<(n)<<"--"<<endl
+#define debx(x) cout << #x << " = " << (x) << endl
+#define deb2(a,b) cout<<#a<<" = "<<(a)<<", "<<#b<<" = "<<(b)<<endl
+#define deb3(a,b,c) cout<<#a<<" = "<<(a)<<", "<<#b<<" = "<<(b)<<", "<<#c<<" = "<<(c)<<endl
+#define debv(v) cout<<#v<<" = "; for(auto x: v) {cout<<x<<", ";} cout<<endl;
+#define debv2(v) for(int i=0; i<v.size(); i++) {cout<<#v<<"["<<i<<"] = "<<v[i]<<endl;}
+ 
+template<typename... T>
+void debm(T&&... args) {
+    ((cout << args << ", "), ...);
+    cout << endl;
+}
+ 
+/*
+    ---- take notes here ----
+    
+*/
+ 
+void solve()
+{
+    ll n,k; cin>>n>>k;
+    vll a,t;
+    readl(a,k);
+    readl(t,k);
+ 
+    vll L(n,inf);
+    vll R(n,inf);
+ 
+    for(ll i=0; i<k; i++)
+    {
+        L[a[i]-1]=t[i];
+        R[a[i]-1]=t[i];
+    }
+ 
+    ll curr = L[0];
+ 
+    for(ll i=1; i<n; i++)
+    {
+        curr = min(curr+1,L[i]);
+        L[i]=curr;
+        // deb2(i,curr);
+    }
+ 
+    curr = R[n-1];
+ 
+    for(ll i=n-2; i>=0; i--)
+    {
+        curr = min(curr+1,R[i]);
+        R[i]=curr;
+        // deb2(i,curr);
+    }
+ 
+    for(ll i=0; i<n; i++)
+    {
+        cout<<min(L[i],R[i])<<" ";
+    }
+    cout<<endl;
+}
+ 
+int main()
+{
+    FAST
+    int t = 1;
+    cin >> t;
+    while(t--)
+    {
+        solve();    
+    }
+ 
+    return 0;
+}
